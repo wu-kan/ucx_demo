9f799d81ba18764eb0aa7a16cee36735be426b93
wu-kan <github@wu-kan.cn>
2025-07-08 17:53:40 +0800

    Update

--- src/messaging.cpp
+++ src/messaging.cpp
@@ -1,48 +1,22 @@
 #include "messaging.h"
 #include <string>
 
-ucp_request_param_t request_parameters {};
+ucp_request_param_t request_parameters{};
 
 ucs_status_t Messaging::on_client_ready() {
-    std::string message = "Hello UCX";
-    auto request = ucp_tag_send_nbx(
-            endpoint,
-            message.c_str(),
-            message.size() + 1,
-            0,
-            &request_parameters
-    );
+  std::string message = "Hello UCX";
+  auto request = ucp_tag_send_nbx(endpoint, message.c_str(), message.size() + 1,
+                                  0, &request_parameters);
 
-    if (auto status = wait_on_request(request); status != UCS_OK) {
-        ucs_error("Sending message failed");
-        return status;
-    }
+  if (auto status = wait_on_request(request); status != UCS_OK) {
+    ucs_error("Sending message failed");
+    return status;
+  }
 
-    ucs_info("Message sent");
-    return UCS_OK;
+  ucs_info("Message sent");
+  return UCS_OK;
 }
 
-#if 1
-ucs_status_t Messaging::on_server_ready() {
-    std::vector<char> message(32);
-    auto request = ucp_tag_recv_nbx(
-            worker,
-            message.data(),
-            message.size(),
-            0,
-            0,
-            &request_parameters
-    );
-
-    if (auto status = wait_on_request(request); status != UCS_OK) {
-        ucs_error("Receiving message failed");
-        return status;
-    }
-
-    ucs_info("Received %s", message.data());
-    return UCS_OK;
-}
-#else
 #define CHECK_CUDA(call)                                                       \
   do {                                                                         \
     const cudaError_t error_code = call;                                       \
@@ -61,17 +35,16 @@ ucs_status_t Messaging::on_server_ready() {
   char *msg_device = nullptr, msg_host[msg_len] = "";
   CHECK_CUDA(cudaMalloc((void **)&msg_device, sizeof(char) * msg_len));
   auto request =
-      ucp_tag_recv_nbx(worker, msg_device, msg_len, 0, 0, &request_parameters);
+      ucp_tag_recv_nbx(worker, msg_host, msg_len, 0, 0, &request_parameters);
 
   if (auto status = wait_on_request(request); status != UCS_OK) {
     ucs_error("Receiving message failed");
     return status;
   }
 
-  CHECK_CUDA(cudaMemcpy((void *)msg_host, (void *)msg_device,
-                        sizeof(char) * msg_len, cudaMemcpyDeviceToHost));
+  // CHECK_CUDA(cudaMemcpy((void *)msg_host, (void *)msg_device,
+  //                       sizeof(char) * msg_len, cudaMemcpyDeviceToHost));
   CHECK_CUDA(cudaFree(msg_device));
   ucs_info("Received %s", msg_host);
   return UCS_OK;
 }
-#endif
